11a98d5b5d78e5ee80cf7553616db7a15ed5c16b
silvinavd <silvinavalverde8@gmail.com>
2021-04-22 20:04:59 +0000

    Estuctura

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var suit = ["♦", "♥", " ♠", "♣"];
+  var number = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let randomsuit = Math.floor(Math.random() * suit.length);
+  let randomnumber = Math.floor(Math.random() * number.length);
+
+  if (suit[randomsuit].length <= 1) {
+    document.getElementById("topsuit").style.color = "red";
+    document.getElementById("bottomsuit").style.color = "red";
+  }
+
+  document.getElementById("topsuit").innerHTML = suit[randomsuit];
+  document.getElementById("bottomsuit").innerHTML = suit[randomsuit];
+  document.getElementById("number").innerHTML = number[randomnumber];
+
+  console.log(suit[randomsuit] + number[randomnumber]);
 };
